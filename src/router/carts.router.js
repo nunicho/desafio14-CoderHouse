0e48158319571d5c9c4717963db31e4256816a56
MauricioAlonso <program.alonso@gmail.com>
2023-11-27 19:53:18 -0300

    listas las rutas del carrito

--- src/router/carts.router.js
+++ src/router/carts.router.js
@@ -1,20 +1,45 @@
 const express = require("express");
-//const mongoose = require("mongoose");
 const router = express.Router();
-//const carritosModelo = require("../dao/DB/models/carritos.modelo.js");
-//const Producto = require("../dao/DB/models/productos.modelo.js");
-//const path = require("path");
-//const prodModelo = require("../dao/DB/models/productos.modelo.js");
 const carritosController = require("../controllers/carritos.controller.js")
 
 
 
 
-router.get("/", carritosController.verCarritos);
+router.get("/", (req, res) => {
+  try {
+    req.logger.info(`Acceso exitoso a la ruta de ver carritos`);
+    carritosController.verCarritos(req, res);
+  } catch (error) {
+    req.logger.error(
+      `Error al acceder a la ruta de ver carritos - Detalle: ${error.message}`
+    );
+    res.status(500).send("Error interno del servidor");
+  }
+});
 
-router.get("/:cid", carritosController.verCarritoConId);
+router.get("/:cid", (req, res) => {
+  try {
+    req.logger.info(`Acceso exitoso a la ruta de ver carrito`);
+    carritosController.verCarritoConId(req, res);
+  } catch (error) {
+    req.logger.error(
+      `Error al acceder a la ruta de ver carrito con ID - Detalle: ${error.message}`
+    );
+    res.status(500).send("Error interno del servidor");
+  }
+});
 
-router.post("/purchase", carritosController.crearCarrito);
+router.post("/purchase", (req, res) => {
+  try {
+    req.logger.info(`Acceso exitoso a la ruta de crear carrito`);
+    carritosController.crearCarrito(req, res);
+  } catch (error) {
+    req.logger.fatal(
+      `Error al acceder a la ruta de crear carrito - Detalle: ${error.message}`
+    );
+    res.status(500).send("Error interno del servidor");
+  }
+});
 
 
 module.exports = router;
